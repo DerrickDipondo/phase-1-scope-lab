3df2c5afac0980e1e46a2ec01ece79eddd612416
DerrickDipondo <derrickdipondo@gmail.com>
2024-12-03 13:38:06 +0300

    scope_lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+const leastFavoriteCustomer = "malala";
+
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "lord";
+}
